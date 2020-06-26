6665d5bd631ad30e31a3d813cb66031f2da9b305
Albert Pérez Farrés <albert.pfarres@gmail.com>
2020-06-26 15:34:45 +0200

    Prevent check if method of agent is async (#11)
    Co-authored-by: Albert Pérez <albert.perez@immfly.com>

--- src/utils/reducer/asyncBus/validator/utils/utils.js
+++ src/utils/reducer/asyncBus/validator/utils/utils.js
@@ -3,13 +3,9 @@ import { METHODS } from '../../static'
 const errorPattern = (method, type) => `The method '${method}' ${type} in the client agent`
 
 export const checkFn = (fn, method) => {
-  if (!fn) {
-    throw new Error(errorPattern(method, 'does not exist'))
-  }
+  if (fn) return
 
-  if (fn() instanceof Promise) return
-
-  throw new Error(errorPattern(method, 'is not an async function'))
+  throw new Error(errorPattern(method, 'does not exist'))
 }
 
 export const checkMethod = (method) => {
